b091e41bd960edf8b7306dbf1375056fdcd6b7f1
Aymane-Elhassouni <aymane.elhassouni3@gmail.com>
2025-09-02 21:31:27 +0100

    l'etulisateur qui enter le rayon de sphere

--- volume-sphere.c
+++ volume-sphere.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int r=10;
+    printf("--- programme calcule volume d'un spheren ---\n");
+
+    int r;
     float pi= 3.14,v;
+    printf("enter le rayon de sphere: ");
+    scanf("%d",&r);
     v = 4.0f/3.0f*(pi*(r*r*r));
     printf("le volume de sphere est: %.2f",v);
     return 0;
