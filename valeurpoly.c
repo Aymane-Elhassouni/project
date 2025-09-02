707dcd751c7d8863a64e35c6277cd96dad593cf2
Aymane-Elhassouni <aymane.elhassouni3@gmail.com>
2025-09-02 22:18:39 +0100

    modifier sur programme qui affichee la valeur d'un polynome

--- valeurpoly.c
+++ valeurpoly.c
@@ -5,7 +5,7 @@ int main(){
     float x;
     printf("enter la valeur de x: ");
     scanf("%f",&x);
-    x = 3*(x*x*x*x*x)+2*(x*x*x*x)-5*(x*x*x)-(x*x)+7*x-6;
+    x = ((((3*x+2)*x-5)*x-1)*x+7)*x-6;
     printf("valeur du polynome est: %.2f",x);
     return 0;
 }
